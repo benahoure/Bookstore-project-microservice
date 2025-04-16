c3bd739fcf53df9807fa232172284c794c64431a
Ben L Ahoure <AHOURE2993@GMAIL.COM>
2025-04-15 19:41:21 -0500

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -64,25 +64,12 @@ module "aws_auth" {
   source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
   version = "~> 20.35.0"
 
-  manage_aws_auth_configmap = false
+  manage_aws_auth_configmap = true
 
   aws_auth_roles = [
     {
-      rolearn  = "arn:aws:iam::66666666666:role/role1"
-      username = "role1"
-      groups   = ["system:masters"]
-    },
-  ]
-
-  aws_auth_users = [
-    {
-      userarn  = "arn:aws:iam::66666666666:user/user1"
-      username = "user1"
-      groups   = ["system:masters"]
-    },
-    {
-      userarn  = "arn:aws:iam::66666666666:user/user2"
-      username = "user2"
+      rolearn  = "arn:aws:iam::992382645606:user/Ben_CloudSpace"
+      username = "Ben_CloudSpace"
       groups   = ["system:masters"]
     },
   ]
